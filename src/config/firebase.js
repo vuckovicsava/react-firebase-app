1b3b94abcd9b8926767c95c765b6a8bb37994eb9
Sava <sava.vuckovic@quantox.com>
2019-01-18 10:58:06 +0100

    connected CreateProject with firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+const config = {
+  apiKey: "AIzaSyAzOm-TNybXP1wvA4DKuCqbw_se8lIOtgY",
+  authDomain: "react-firebase-tutorials.firebaseapp.com",
+  databaseURL: "https://react-firebase-tutorials.firebaseio.com",
+  projectId: "react-firebase-tutorials",
+  storageBucket: "react-firebase-tutorials.appspot.com",
+  messagingSenderId: "726749153692"
+};
+
+firebase.initializeApp(config);
+
+const firestore = firebase.firestore();
+// firestore.settings({ timestampsInSnapshots: true });
+
+export default firestore;
--- src/store/actions/project.js
+++ src/store/actions/project.js
@@ -1,5 +1,19 @@
+import firestore from '../../config/firebase';
+
 export const createProject = project => {
   return (dispatch, getState) => {
-    dispatch({ type: 'CREATE_PROJECT', project });
+    firestore.collection('projects').add({
+      ...project,
+      authorFirstName: 'Sava',
+      authorLastName: 'Vuckovic',
+      authorId: 12345,
+      createdAt: new Date()
+    })
+    .then(() => {
+      dispatch({ type: 'CREATE_PROJECT', project })
+    })
+    .catch(err => {
+      dispatch({ type: 'CREATE_PROJECT_ERROR', err })
+    })
   }
 }
